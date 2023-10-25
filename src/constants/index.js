3ecde73382f318b72b3f6271e988196dcd1e9295
Louis <louisapstg@gmail.com>
2023-10-25 13:49:50 +0700

    add global nav item

---
+++ src/constants/index.js
@@ -0,0 +1,37 @@
+export const navLinks = [
+   {
+      id: 1,
+      title: 'HOME',
+   },
+   {
+      id: 2,
+      title: 'MOVIE',
+   },
+   {
+      id: 3,
+      title: 'TV SHOW',
+   },
+   {
+      id: 4,
+      title: 'WEB SERIES',
+   },
+   {
+      id: 5,
+      title: 'PREMIUM',
+   },
+]
+
+export const upcomingMoviesLinks = [
+   {
+      id: 10,
+      title: 'Movies',
+   },
+   {
+      id: 11,
+      title: 'TVs Shows',
+   },
+   {
+      id: 12,
+      title: 'Animes',
+   },
+]
