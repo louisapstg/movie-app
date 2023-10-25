85f317522d1b57fa23c903854ac112d749e997af
Louis <louisapstg@gmail.com>
2023-10-25 13:47:23 +0700

    add extended class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            'highlight': '#CCFF00',
+            'soft-gray': '#2E2E2E',
+         },
+      },
+      screens: {
+         ss: "480px",
+         sm: "620px",
+         md: "768px", //tablets
+         lg: "1060px", //desktops
+         xl: "1200px", // xl desktops
+      },
+   },
+   plugins: [],
 };
