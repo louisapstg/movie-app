9098fa3c4c55d6ca9024aa681269c72ce582b613
Louis <louisapstg@gmail.com>
2023-10-25 13:50:08 +0700

    add hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import HeroTitle from "./HeroTitle";
+import HeroFooter from "./HeroFooter";
+
+const Hero = () => {
+	return (
+		<section className="flex flex-col md:flex-row mx-auto py-6 md:py-16 bg-gradient-to-b from-black to-soft-gray">
+			<div className="flex flex-1 flex-col text-center lg:text-left px-6 md:px-16 xl:px-0 justify-center items-center lg:items-start text-white mt-40">
+				<HeroTitle />
+				<HeroFooter />
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
---
+++ src/components/Hero/HeroFooter.jsx
@@ -0,0 +1,21 @@
+import playIcon from "../../assets/play-symbol.svg";
+
+const HeroFooter = () => {
+	return (
+		<>
+			<div className="flex flex-row pt-2">
+				<div className="bg-white rounded-sm text-black pt-0.5 px-2 mx-2">Movie</div>
+				<div className="border rounded-sm border-white pt-0.5 px-2 mx-2">HD</div>
+				<div className="pt-0.5 px-2">All Genre</div>
+			</div>
+			<button className="flex mt-6 pl-2 pr-4 py-2 border-2 border-highlight rounded-full font-bold items-center hover:bg-soft-gray hover:text-highlight transition-all">
+				<svg width="24" height="24">
+					<image href={playIcon} />
+				</svg>
+				PLAY NOW
+			</button>
+		</>
+	);
+};
+
+export default HeroFooter;
