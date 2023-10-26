3ff7b9196473be2ef63ad54f22a0fe48af95fea3
Louis <louisapstg@gmail.com>
2023-10-26 16:27:26 +0700

    update components exporter

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar/Navbar';
 import Hero from './Hero/Hero';
 import UpcomingMovies from './UpcomingMovies/UpcomingMovies';
+import TrendingMovies from './PopularMovies/PopularMovies';
+import Footer from './Footer/Footer';
 
-export { Navbar, Hero, UpcomingMovies }
+export { Navbar, Hero, UpcomingMovies, TrendingMovies, Footer }
