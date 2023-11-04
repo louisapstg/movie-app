9ccfffe9942d9b7f8e6a05b41cd93f85d32f8a9c
Louis <louisapstg@gmail.com>
2023-11-04 23:38:54 +0700

    add movies page

---
+++ src/pages/Primary/MoviesPage.jsx
@@ -0,0 +1,13 @@
+import { Footer, Movies, Navbar } from "../../components";
+
+const MoviesPage = () => {
+	return (
+		<>
+			<Navbar />
+			<Movies />
+			<Footer />
+		</>
+	);
+};
+
+export default MoviesPage;
