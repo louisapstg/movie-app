c2ceaf9d5a78afdc57233bbf16277a183ab012d9
Louis <louisapstg@gmail.com>
2023-11-04 23:39:46 +0700

    move home page

---
+++ src/pages/Primary/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Hero, UpcomingMovies, TrendingMovies, Footer } from "../../components";
+
+const Home = () => {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<UpcomingMovies />
+			<TrendingMovies />
+			<Footer />
+		</>
+	);
+};
+
+export default Home;
