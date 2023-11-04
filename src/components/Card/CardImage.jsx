343299798ad4f70af164a8cd7b3acd2ae8bb6bce
Louis <louisapstg@gmail.com>
2023-11-04 23:41:05 +0700

    update card component

--- src/components/Card/CardImage.jsx
+++ src/components/Card/CardImage.jsx
@@ -2,13 +2,11 @@
 const CardImage = ({ data }) => {
 	const { poster_path } = data || {};
 	return (
-		<a href="#">
-			<img
-				className="rounded-t-lg max-h-[337px]"
-				src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-				alt=""
-			/>
-		</a>
+		<img
+			className="rounded-t-lg max-h-[337px]"
+			src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+			alt=""
+		/>
 	);
 };
 
