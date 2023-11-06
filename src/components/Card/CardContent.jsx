90caed0c209cd7e101b0eeb6b7b4e52006a9bd23
Louis <louisapstg@gmail.com>
2023-11-06 13:47:51 +0700

    update cardcontent data fetching variable

--- src/components/Card/CardContent.jsx
+++ src/components/Card/CardContent.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 const CardContent = ({ data }) => {
-	const { title, release_date } = data || {};
-	const date = new Date(release_date);
+	const { title, name, release_date, first_air_date } = data || {};
+	const date = new Date(release_date || first_air_date);
 	const year = date.getFullYear();
 	return (
 		<div className="p-2 text-white">
 			<div className="grid grid-rows-2 gap-0 truncate">
-				<h5 className="mb-2 text-base md:text-lg tracking-tight truncate">{title}</h5>
+				<h5 className="mb-2 text-base md:text-lg tracking-tight truncate">
+					{title || name}
+				</h5>
 				<div className="text-sm md:text-base text-highlight">{year}</div>
 			</div>
 		</div>
