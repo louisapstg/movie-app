360d1b61f8644778c931eaa834c4a0d494b15254
Louis <louisapstg@gmail.com>
2023-11-17 17:14:36 +0700

    update minor detail in hero component

--- src/components/Hero/HeroTitle.jsx
+++ src/components/Hero/HeroTitle.jsx
@@ -2,7 +2,7 @@ const HeroTitle = () => {
 	return (
 		<>
 			<div className="text-highlight text-xl lg:text-3xl leading-loose">Filmagnet</div>
-			<div className="font-semibold text-3xl lg:text-6xl tracking-wide lg:hover:tracking-wider transition-all leading-tight">
+			<div className="font-semibold text-3xl lg:text-6xl tracking-tighter lg:hover:tracking-wider transition-all leading-tight">
 				Unlimited <span className="text-highlight">Entertainment</span>, <br /> Movies, TVs
 				shows, & More.
 			</div>
