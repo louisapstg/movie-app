b1bfdf4f57c15c30b41d23a29d39e8a49d12e30d
Louis <louisapstg@gmail.com>
2023-11-17 17:18:01 +0700

    add sub title for detailed page

---
+++ src/components/Detail/DetailSubTitle.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import watchTime from "../../assets/svg/watch-later.svg";
+import calender from "../../assets/svg/calender.svg";
+const DetailSubTitle = ({ genres, runtime, year, url, number_of_episodes }) => {
+	return (
+		<div className="flex items-center p-3">
+			<div className="bg-white rounded-sm text-black px-2 mx-2 uppercase">{url}</div>
+			<div className="border rounded-sm border-white px-2 mx-2">HD</div>
+			<ul className="list-none flex justify-center items-center">
+				{genres?.map((genre) => {
+					return (
+						<li key={genre.id} className="mx-1">
+							{genre === genres[genres.length - 1]
+								? `${genre.name}`
+								: `${genre.name},`}
+						</li>
+					);
+				})}
+			</ul>
+			<div className="flex mx-2 items-center">
+				<svg width={18} height={18} className="mr-1">
+					<image href={watchTime} />
+				</svg>
+				{runtime ? `${runtime} min` : `${number_of_episodes} episodes`}
+			</div>
+			<div className="flex mx-2 items-center">
+				<svg width={18} height={18} className="mr-1">
+					<image href={calender} />
+				</svg>
+				{year}
+			</div>
+		</div>
+	);
+};
+
+export default DetailSubTitle;
