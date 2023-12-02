780106aae2911d88aed5bb65af43bd8654b09146
Louis <louisapstg@gmail.com>
2023-12-02 11:41:45 +0700

    add genres and pagination to reducer

--- src/stores/features/moviesSlice.js
+++ src/stores/features/moviesSlice.js
@@ -3,38 +3,82 @@ import MoviesAPI from './../../apis/movies.api';
 
 
 const initialState = {
-   data: [],
-   status: "idle",
-   error: null,
-   loading: false
+    data: [],
+    genres: [],
+    total_pages: 0,
+    status: "idle",
+    error: null,
+    loading: false
 }
 
-export const fetchMovies = createAsyncThunk("fetch/movies", async () => {
-   try {
-      const response = await MoviesAPI.getMovies();
-      return response.data.results;
-   } catch (e) {
-      throw Error(e)
-   }
+export const fetchMovies = createAsyncThunk("fetch/movies", async (page) => {
+    try {
+        const response = await MoviesAPI.getMovies(page);
+        return response.data;
+    } catch (e) {
+        throw Error(e)
+    }
+})
+
+export const searchMovies = createAsyncThunk("fetch/movies/search", async (keyword, page) => {
+    try {
+        const response = await MoviesAPI.searchMovie(keyword, page);
+        return response.data;
+    } catch (e) {
+        throw Error(e)
+    }
+})
+
+export const genresMovies = createAsyncThunk("fetch/movies/genres", async () => {
+    try {
+        const response = await MoviesAPI.genresMovie()
+        return response.data
+    } catch (e) {
+        throw Error(e)
+    }
 })
 
 const moviesSlice = createSlice({
-   name: 'movies',
-   initialState,
-   extraReducers: (builder) => {
-      builder
-         .addCase(fetchMovies.pending, (state) => {
-            state.status = "loading";
-         })
-         .addCase(fetchMovies.fulfilled, (state, action) => {
-            state.status = "succeeded";
-            state.data = action.payload;
-         })
-         .addCase(fetchMovies.rejected, (state, action) => {
-            state.status = "failed";
-            state.error = action.error.message;
-         })
-   }
+    name: 'movies',
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchMovies.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(fetchMovies.fulfilled, (state, action) => {
+                state.status = "succeeded";
+                state.data = action.payload.results;
+                state.total_pages = action.payload.total_pages
+            })
+            .addCase(fetchMovies.rejected, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            })
+            .addCase(searchMovies.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(searchMovies.fulfilled, (state, action) => {
+                state.status = "succeeded";
+                state.data = action.payload.results;
+                state.total_pages = action.payload.total_pages
+            })
+            .addCase(searchMovies.rejected, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            })
+            .addCase(genresMovies.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(genresMovies.fulfilled, (state, action) => {
+                state.status = "succeeded";
+                state.genres = action.payload.genres;
+            })
+            .addCase(genresMovies.rejected, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            })
+    }
 })
 
 export default moviesSlice.reducer;
