e4a75dc44cba7d901afd5102d9f9478364b318cc
Louis <louisapstg@gmail.com>
2023-12-08 18:19:14 +0700

    add sorting array

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,55 +1,108 @@
 export const navLinks = [
-   {
-      id: 1,
-      title: 'HOME',
-      link: '/'
-   },
-   {
-      id: 2,
-      title: 'MOVIES',
-      link: '/movies'
-   },
-   {
-      id: 3,
-      title: 'TV SHOW',
-      link: '/tv-show'
-   },
+    {
+        id: 1,
+        title: 'HOME',
+        link: '/'
+    },
+    {
+        id: 2,
+        title: 'MOVIES',
+        link: '/movies'
+    },
+    {
+        id: 3,
+        title: 'TV SHOW',
+        link: '/tv-show'
+    },
 ]
 
 export const upcomingMoviesLinks = [
-   {
-      id: 10,
-      title: 'Movies',
-   },
-   {
-      id: 11,
-      title: 'TVs Shows',
-   },
-   {
-      id: 12,
-      title: 'Animes',
-   },
+    {
+        id: 10,
+        title: 'Movies',
+    },
+    {
+        id: 11,
+        title: 'TVs Shows',
+    },
+    {
+        id: 12,
+        title: 'Animes',
+    },
 ]
 
 export const footerLinks = [
-   {
-      id: 1,
-      title: 'About us',
-   },
-   {
-      id: 2,
-      title: 'Vlog',
-   },
-   {
-      id: 3,
-      title: 'Contact',
-   },
-   {
-      id: 4,
-      title: 'Report broken links',
-   },
-   {
-      id: 5,
-      title: 'Disclaimer',
-   },
+    {
+        id: 1,
+        title: 'About us',
+    },
+    {
+        id: 2,
+        title: 'Vlog',
+    },
+    {
+        id: 3,
+        title: 'Contact',
+    },
+    {
+        id: 4,
+        title: 'Report broken links',
+    },
+    {
+        id: 5,
+        title: 'Disclaimer',
+    },
+]
+
+export const sorting = [
+    {
+        id: 1,
+        sort_by: "popularity.asc",
+        label: "Popularity Ascending"
+    },
+    {
+        id: 2,
+        sort_by: "popularity.desc",
+        label: "Popularity Descending"
+    },
+    {
+        id: 3,
+        sort_by: "revenue.asc",
+        label: "Revenue Ascending"
+    },
+    {
+        id: 4,
+        sort_by: "revenue.desc",
+        label: "Revenue Descending"
+    },
+    {
+        id: 5,
+        sort_by: "primary_release_date.asc",
+        label: "Primary Release Date Ascending"
+    },
+    {
+        id: 6,
+        sort_by: "primary_release_date.desc",
+        label: "Primary Release Date Descending"
+    },
+    {
+        id: 7,
+        sort_by: "vote_average.asc",
+        label: "Vote Average Ascending"
+    },
+    {
+        id: 8,
+        sort_by: "vote_average.desc",
+        label: "Vote Average Descending"
+    },
+    {
+        id: 9,
+        sort_by: "vote_count.asc",
+        label: "Vote Count Ascending"
+    },
+    {
+        id: 10,
+        sort_by: "vote_count.desc",
+        label: "Vote Count Descending"
+    },
 ]
