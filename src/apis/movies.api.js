a4f50b8835f579ffb98b8d8d21882f39c56d6e05
Louis <louisapstg@gmail.com>
2023-12-08 18:16:29 +0700

    add genreId and sortBy to api request

--- src/apis/movies.api.js
+++ src/apis/movies.api.js
@@ -1,33 +1,33 @@
 import { axiosInstance } from "../configs/axiosInstance";
 
 const MoviesAPI = {
-   async getMovies(page) {
-      try {
-         const response = await axiosInstance.get(`/discover/movie?page=${page}`)
-         return response
-      } catch (error) {
-         const { message } = error.response.data;
-         throw new Error(message)
-      }
-   },
-   async searchMovie(keyword, page) {
-      try {
-         const response = await axiosInstance.get(`/search/movie?query=${keyword}&page=${page}`)
-         return response
-      } catch (err) {
-         const { message } = err.response.data;
-         throw new Error(message)
-      }
-   },
-   async genresMovie() {
-      try {
-         const response = await axiosInstance.get(`/genre/movie/list`)
-         return response
-      } catch (err) {
-         const { message } = err.response.data;
-         throw new Error(message)
-      }
-   }
+    async getMovies(genreId, page, sortBy) {
+        try {
+            const response = await axiosInstance.get(`/discover/movie?page=${page}&with_genres=${genreId}&sort_by=${sortBy}`);
+            return response
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message)
+        }
+    },
+    async searchMovie(keyword, page) {
+        try {
+            const response = await axiosInstance.get(`/search/movie?query=${keyword}&page=${page}`)
+            return response
+        } catch (err) {
+            const { message } = err.response.data;
+            throw new Error(message)
+        }
+    },
+    async genresMovie() {
+        try {
+            const response = await axiosInstance.get(`/genre/movie/list`)
+            return response
+        } catch (err) {
+            const { message } = err.response.data;
+            throw new Error(message)
+        }
+    }
 }
 
 export default MoviesAPI
--- src/apis/tv.api.js
+++ src/apis/tv.api.js
@@ -1,33 +1,33 @@
 import { axiosInstance } from "../configs/axiosInstance"
 
 const TvAPI = {
-   async getTvShow(page) {
-      try {
-         const response = await axiosInstance.get(`/discover/tv?page=${page}`);
-         return response;
-      } catch (error) {
-         const { message } = error.response.data
-         throw new Error(message)
-      }
-   },
-   async searchTvShow(keyword, page) {
-      try {
-         const response = await axiosInstance.get(`/search/tv?query=${keyword}&page=${page}`)
-         return response
-      } catch (error) {
-         const { message } = error.response.data
-         throw new Error(message)
-      }
-   },
-   async genresTV() {
-      try {
-         const response = await axiosInstance.get(`/genre/tv/list`)
-         return response
-      } catch (error) {
-         const { message } = error.response.data
-         throw new Error(message)
-      }
-   }
+    async getTvShow(genreId, page, sortBy) {
+        try {
+            const response = await axiosInstance.get(`/discover/tv?page=${page}&with_genres=${genreId}&sort_by=${sortBy}`);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data
+            throw new Error(message)
+        }
+    },
+    async searchTvShow(keyword, page) {
+        try {
+            const response = await axiosInstance.get(`/search/tv?query=${keyword}&page=${page}`)
+            return response
+        } catch (error) {
+            const { message } = error.response.data
+            throw new Error(message)
+        }
+    },
+    async genresTV() {
+        try {
+            const response = await axiosInstance.get(`/genre/tv/list`)
+            return response
+        } catch (error) {
+            const { message } = error.response.data
+            throw new Error(message)
+        }
+    }
 }
 
 export default TvAPI
