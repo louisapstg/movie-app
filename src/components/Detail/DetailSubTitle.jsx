fb7fafd2e192981773b07e92a97304cdd6f315e1
Louis <louisapstg@gmail.com>
2023-12-11 23:15:12 +0700

    update to responsive layout

--- src/components/Detail/DetailSubTitle.jsx
+++ src/components/Detail/DetailSubTitle.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable react/prop-types */
 import watchTime from "../../assets/svg/watch-later.svg";
 import calender from "../../assets/svg/calender.svg";
-const DetailSubTitle = ({ genres, runtime, year, url, number_of_episodes }) => {
+const DetailSubTitle = ({ genres, runtime, year, url, number_of_episodes, original_language }) => {
 	return (
-		<div className="flex items-center p-3">
-			<div className="bg-white rounded-sm text-black px-2 mx-2 uppercase">{url}</div>
-			<div className="border rounded-sm border-white px-2 mx-2">HD</div>
-			<ul className="list-none flex justify-center items-center">
+		<div className="flex flex-col lg:flex-row lg:items-center p-1 lg:p-3">
+			<div className="flex mb-3">
+				<div className="bg-white rounded-sm text-black px-2 mx-2 uppercase">{url}</div>
+				<div className="border rounded-sm border-white px-2 mx-2">HD</div>
+				<div className="border rounded-sm border-white px-2 mx-2 uppercase">
+					{original_language}
+				</div>
+			</div>
+			<ul className="list-none flex justify-center items-center mx-6 border-b">
 				{genres?.map((genre) => {
 					return (
 						<li key={genre.id} className="mx-1">
@@ -17,7 +22,7 @@ const DetailSubTitle = ({ genres, runtime, year, url, number_of_episodes }) => {
 					);
 				})}
 			</ul>
-			<div className="flex mx-2 items-center">
+			<div className="flex m-2 items-center">
 				<svg width={18} height={18} className="mr-1">
 					<image href={watchTime} />
 				</svg>
