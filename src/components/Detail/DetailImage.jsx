282454637213e393de181a94d1d190ab3e4e330e
Louis <louisapstg@gmail.com>
2023-12-11 23:14:53 +0700

    update to responsive layout

--- src/components/Detail/DetailImage.jsx
+++ src/components/Detail/DetailImage.jsx
@@ -2,9 +2,9 @@
 const DetailImage = ({ poster_path, original_title }) => {
 	return (
 		<img
-			src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+			src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
 			alt={original_title}
-			className="max-h-[426px] h-52 lg:h-full md:mt-5 md:mb-20"
+			className="w-full md:w-auto md:max-h-[426px] lg:h-full md:mt-5 md:mb-20"
 		/>
 	);
 };
