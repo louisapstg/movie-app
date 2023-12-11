f1cdb8fc1784b1f6775d4587d480ca611a74dabb
Louis <louisapstg@gmail.com>
2023-12-11 23:15:36 +0700

    update detail component

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -5,8 +5,7 @@ import DetailAPI from "../../apis/detail.api";
 import DetailImage from "./DetailImage";
 import DetailTitle from "./DetailTitle";
 import DetailSubTitle from "./DetailSubTitle";
-import star from "../../assets/svg/star.svg";
-import people from "../../assets/svg/people.svg";
+import Review from "./Review";
 
 const Detail = ({ id, url }) => {
 	const [data, setData] = useState([]);
@@ -17,9 +16,10 @@ const Detail = ({ id, url }) => {
 		});
 	}, [id, url]);
 
+	console.log(data);
+
 	const {
 		backdrop_path,
-		belongs_to_collection,
 		budget,
 		genres,
 		homepage,
@@ -29,8 +29,6 @@ const Detail = ({ id, url }) => {
 		overview,
 		popularity,
 		poster_path,
-		production_companies,
-		production_countries,
 		release_date,
 		first_air_date,
 		revenue,
@@ -42,12 +40,9 @@ const Detail = ({ id, url }) => {
 		vote_count,
 	} = data;
 
-	const date = new Date(release_date || first_air_date);
-	const year = date.getFullYear();
-
 	const backgroundStyle = {
 		backgroundImage: backdrop_path
-			? `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)), url('https://image.tmdb.org/t/p/original${backdrop_path}')`
+			? `linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.9)), url('https://image.tmdb.org/t/p/original${backdrop_path}')`
 			: "none",
 		backgroundSize: "cover",
 		backgroundPosition: "center",
@@ -56,33 +51,27 @@ const Detail = ({ id, url }) => {
 	return (
 		<section
 			style={backgroundStyle}
-			className="flex flex-row md:flex-row mx-auto p-6 md:p-16 md:mt-16 bg-gradient-to-b from-black to-soft-gray text-white"
+			className="flex flex-col md:flex-row mx-auto p-6 mt-16 md:p-16 md:mt-16 bg-gradient-to-b from-black to-soft-gray text-white"
 		>
 			<DetailImage poster_path={poster_path} original_title={original_title} />
-			<div className="flex flex-col p-12">
-				<DetailTitle original_title={original_title} name={name} />
+			<div className="flex flex-col p-1 lg:p-12">
+				<DetailTitle original_title={original_title} homepage={homepage} name={name} />
 				<DetailSubTitle
 					genres={genres}
+					original_language={original_language}
 					runtime={runtime}
-					year={year}
+					year={release_date || first_air_date}
 					url={url}
 					number_of_episodes={number_of_episodes}
 				/>
 				<div className="p-5">{overview}</div>
-				<div className="px-5 flex items-center text-2xl">
-					<div className="flex items-center">
-						<svg width={36} height={36} className="mr-2">
-							<image href={star} />
-						</svg>
-						{vote_average}
-					</div>
-					<div className="flex items-center">
-						<svg width={48} height={48} className="ml-2">
-							<image href={people} />
-						</svg>
-						{vote_count}
-					</div>
-				</div>
+				<div className="p-5">&quot;{tagline}&quot;</div>
+				<Review
+					popularity={popularity}
+					vote_average={vote_average}
+					vote_count={vote_count}
+				/>
+				<div className="p-5 font-extrabold text-highlight uppercase text-sm">{status}</div>
 			</div>
 		</section>
 	);
