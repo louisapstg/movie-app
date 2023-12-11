ea00e7ded5274e59f37c661385ce73d4ff2be672
Louis <louisapstg@gmail.com>
2023-12-11 23:15:25 +0700

    add new href to the a tag

--- src/components/Detail/DetailTitle.jsx
+++ src/components/Detail/DetailTitle.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable react/prop-types */
-const DetailTitle = ({ original_title, name }) => {
+const DetailTitle = ({ original_title, name, homepage }) => {
 	return (
-		<div className="text-highlight text-6xl p-3 text-gradient font-bold">
-			{original_title || name}
+		<div>
+			{homepage ? (
+				<a
+					href={homepage}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-highlight text-2xl md:text-4xl lg:text-6xl p-3 text-gradient font-bold"
+				>
+					{original_title || name}
+				</a>
+			) : (
+				<div className="text-highlight text-2xl md:text-4xl lg:text-6xl p-3 text-gradient font-bold">
+					{original_title || name}
+				</div>
+			)}
 		</div>
 	);
 };
